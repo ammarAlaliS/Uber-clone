1791199f6de55109391156a16c450c70a89680a9
ammarAlaliS <ammaralialiwa@gmail.com>
2024-05-18 20:55:45 -0600

    add naviagion form array of an objects

--- App.js
+++ App.js
@@ -4,19 +4,38 @@ import { Provider } from 'react-redux';
 import store from './store';
 import HomeScreen from './screens/HomeScreen';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import MapScreen from './screens/MapScreen';
 
 // 1) paso numero uno antes de arrancar el proyecto set up redux npm install @reduxjs/toolkit
 // 2) agregar tailwind-react-native-classnames from https://www.nativewind.dev/quick-starts/expo
 // 3) se agrego  "react-native-elements": "^3.4.3", "react-native-safe-area-context": "^4.10.1", "react-native-vector-icons": "^10.1.0",from https://reactnativeelements.com/docs/installation
+
+const Stack = createStackNavigator();
 export default function App() {
   return (
-
     <Provider store={store}>
-      <SafeAreaProvider>
-        <HomeScreen />
-      </SafeAreaProvider>
+      <NavigationContainer>
+        <SafeAreaProvider>
+          <Stack.Navigator>
+            <Stack.Screen
+              name="HomeScreen"
+              component={HomeScreen}
+              options={{
+                headerShown: false
+              }}
+            />
+            <Stack.Screen
+              name="MapScreen"
+              component={MapScreen}
+              options={{
+                headerShown: false
+              }}
+            />
+          </Stack.Navigator>
+        </SafeAreaProvider>
+      </NavigationContainer>
     </Provider>
-
   );
 }
-
--- components/NavOptions.js
+++ components/NavOptions.js
@@ -1,3 +1,4 @@
+import { useNavigation } from '@react-navigation/native';
 import { Icon } from '@rneui/base';
 import React from 'react';
 import { View, Text, FlatList, TouchableOpacity, Image } from 'react-native';
@@ -16,17 +17,21 @@ const data = [
         image: "https://links.papareact.com/2Bw",
         screen: "MapScreen",
     },
-    
+
 ]
 
 const NavOptions = () => {
+    const navigation = useNavigation()
     return (
         <FlatList
             data={data}
             horizontal
             contentContainerStyle={{ flexGrow: 1 }}
             renderItem={({ item }) => (
-                <TouchableOpacity className=" p-2 pl-6 pb-8 bg-gray-200 m-2 w-40">
+                <TouchableOpacity
+                    onPress={()=> navigation.navigate(item.screen)}
+                    className=" p-2 pl-6 pb-8 bg-gray-200 m-2 w-40"
+                    >
                     <View>
                         <Image
                             source={{ uri: item.image }}
@@ -34,12 +39,12 @@ const NavOptions = () => {
                         />
                         <Text className=" mt-2 text-lg font-semibold">{item.title}</Text>
                         <Icon style={{
-                            padding: 8, 
-                            backgroundColor: '#000', 
-                            borderRadius: '50%', 
-                            width: 40, 
-                            marginTop:8
-                        }}  name='arrowright' color="white" type='antdesign'/>
+                            padding: 8,
+                            backgroundColor: '#000',
+                            borderRadius: 9999,
+                            width: 40,
+                            marginTop: 8
+                        }} name='arrowright' color="white" type='antdesign' />
                     </View>
                 </TouchableOpacity>
             )}
